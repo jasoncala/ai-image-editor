948793764b1cc25eab6ae9ea25ebdf94c77aa81b
jasoncala <calaj@uwindsor.ca>
2024-10-17 18:03:59 -0400

    Added more AI image features

--- components/toolbar/image-toolbar.tsx
+++ components/toolbar/image-toolbar.tsx
@@ -1,9 +1,15 @@
+import AIBackgroundReplace from "./bg-replace"
+import ExtractPart from "./extract-part"
 import GenRemove from "./gen-remove"
+import GenerativeFill from "./generative-fill"
 
 export default function ImageTools() {
   return (
     <>
       <GenRemove />
+      <AIBackgroundReplace />
+      <GenerativeFill />
+      <ExtractPart/>
     </>
   )
 }
