6b98e1c3ca1064cc82b86f22f1c484b347e38a5a
jasoncala <calaj@uwindsor.ca>
2024-10-17 14:29:46 -0400

    Dark/light mode + some styling

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,12 +1,21 @@
 'use client'
 
+import { ModeToggle } from "@/components/theme/mode-toggle"
+import Layers from "./layers/layers"
 import UploadImage from "./upload/upload-image"
+import { useLayerStore } from "@/lib/layer-store"
 
 export default function Editor() {
+    const activeLayer = useLayerStore((state) => state.activeLayer)
     return(
-        <div>
-            <h1>Editor</h1>
+        <div className="flex h-full ">
+            <div className="py-6 px-4 basis-[240px] shrink-0">
+                <div className="pb-12 text-center">
+                    <ModeToggle/>
+                </div>
+            </div>
             <UploadImage/>
+            <Layers/>
         </div>
     )
 }
