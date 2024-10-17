4a22aa3f24fd4dc8969a55b6fe24ecc55f5846a1
jasoncala <calaj@uwindsor.ca>
2024-10-17 13:08:35 -0400

    Starting components

---
+++ components/editor.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Editor() {
+    return(
+        <div>
+            <h1>Editor</h1>
+        </div>
+    )
+}
---
+++ components/upload/upload-image.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function uploadImage(){
+    return (
+        <div>
+            <h1>Upload Image</h1>
+        </div>
+    )
+}
